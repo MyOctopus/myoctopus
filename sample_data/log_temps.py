229851d6b7582c7aa45ce415d4eeb1388956dfe2
root <root@raspberrypi.(none)>
2015-03-01 18:31:12 +0000

    Added newlines to log file

--- sample_data/log_temps.py
+++ sample_data/log_temps.py
@@ -3,12 +3,13 @@ import os
 import time
 import pprint
 
-sensors = [id for id in os.listdir('/sys/bus/w1/devices/') if id.startswith('28-')]
+BUS_DIR = '/sys/bus/w1/devices'
+sensors = [id for id in os.listdir(BUS_DIR) if id.startswith('28-')]
 
 def get_temps(sensors):
     out = []
     for s in sensors:
-        tf = open('/sys/bus/w1/devices/%s/w1_slave' % (s,))
+        tf = open('%s/%s/w1_slave' % (BUS_DIR,s))
         txt = tf.read()
         tf.close()
         data = int(txt.split('\n')[1].split('t=')[-1])
@@ -19,7 +20,7 @@ while(True):
     temps = get_temps(sensors)
     print [t[-1] for t in temps]
     with open(str(datetime.date.today()) + '.txt', "a") as fl:
-        fl.write(str(temps))
+        fl.write(str(temps) + '\n')
     time.sleep(1)
 
 
