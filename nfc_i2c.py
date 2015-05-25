b1115f93899f6f5db223a73980139df5a127f01d
silvh <sylwester.h@gmail.com>
2015-05-25 20:26:44 +0100

    Update nfc_i2c.py

--- nfc_i2c.py
+++ nfc_i2c.py
@@ -2,6 +2,6 @@
   i2C = mraa.I2c(1); #i2c bus choice
   i2C.address(0x28); #NFC address 0x28 on i2c
 
-  i2C.write(bytearray(b'\xFF\xFE\x02\x00')); #16bit register address FFEE and put values 0x02, 0x01 (turn on NFC module)
+  i2C.write(bytearray(b'\xFF\xFE\x02\x00')); #16bit register address FFFE and put values 0x02, 0x01 (turn on NFC module)
   d = i2C.read(200); #read memory started from 0x0000 follow by 0x00
   print d;
