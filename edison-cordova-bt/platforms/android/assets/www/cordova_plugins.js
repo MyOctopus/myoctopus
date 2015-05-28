e69ec0b4532dcf24b7e736d0e1236f118391580b
mpaun <mihaipaun01@gmail.com>
2015-05-28 21:02:06 +0100

    Added Android platform

---
+++ edison-cordova-bt/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,24 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.megster.cordova.bluetoothserial/www/bluetoothSerial.js",
+        "id": "com.megster.cordova.bluetoothserial.bluetoothSerial",
+        "clobbers": [
+            "window.bluetoothSerial"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.megster.cordova.bluetoothserial": "0.4.3",
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.apache.cordova.console": "0.2.13"
+}
+// BOTTOM OF METADATA
+});
