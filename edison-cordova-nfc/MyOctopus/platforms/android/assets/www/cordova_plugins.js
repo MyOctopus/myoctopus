feaf71a9a56769884af0edf1ee464f4ff066db5b
Chmely <petr.chmelar@gmail.com>
2015-05-28 23:17:25 +0100

    First iteration

---
+++ edison-cordova-nfc/MyOctopus/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,71 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.chariotsolutions.nfc.plugin/www/phonegap-nfc.js",
+        "id": "com.chariotsolutions.nfc.plugin.NFC",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.megster.cordova.ble/www/ble.js",
+        "id": "com.megster.cordova.ble.ble",
+        "clobbers": [
+            "ble"
+        ]
+    },
+    {
+        "file": "plugins/hu.dpal.phonegap.plugins.SpinnerDialog/www/spinner.js",
+        "id": "hu.dpal.phonegap.plugins.SpinnerDialog.SpinnerDialog",
+        "merges": [
+            "window.plugins.spinnerDialog"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.chariotsolutions.nfc.plugin": "0.6.1",
+    "com.megster.cordova.ble": "0.1.7",
+    "hu.dpal.phonegap.plugins.SpinnerDialog": "1.3.1",
+    "org.apache.cordova.device": "0.3.0",
+    "org.apache.cordova.dialogs": "0.3.0",
+    "org.apache.cordova.statusbar": "0.1.10",
+    "org.apache.cordova.vibration": "0.3.13"
+}
+// BOTTOM OF METADATA
+});
---
+++ edison-cordova-nfc/MyOctopus/www/scripts/index.js
@@ -0,0 +1,126 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    var MyOctopus = function () {
+        var self = this;
+
+        self.onDeviceReady = function() {
+            // Handle the Cordova pause and resume events
+            document.addEventListener('pause', window.myoctopus.onPause.bind(self), false);
+            document.addEventListener('resume', window.myoctopus.onResume.bind(self), false);
+
+            // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+            self.nfcChanged();
+        };
+
+        self.onPause = function() {
+            // TODO: This application has been suspended. Save application state here.
+        };
+
+        self.onResume = function() {
+            // TODO: This application has been reactivated. Restore application state here.
+        };
+
+        self.nfcEnabled = false;
+        self.nfcChanged = function() {
+            //if (nfcSwitch.isChecked() && !nfcEnabled) {
+            if (!self.nfcEnabled) {
+                nfc.enabled(
+                    function () {                   // success, NFC is enabled on the phone
+                        nfc.addNdefListener(
+                            self.nfcHandler,
+                            function () {           // success
+                                self.nfcEnabled = true;
+                                alert('nfc enabled');
+                            },
+                            function (error) {      /* error */
+                                nfcSwitch.setChecked(false);
+                                document.getElementById('settingsError').textContent = error;
+                            }
+                        );
+                    },
+                    // msg is one of NO_NFC (no hardware support) or NFC_DISABLED (supported but disabled)
+                    function (msg) {
+                        document.getElementById('settingsError').textContent = msg;
+                        nfcSwitch.setChecked(false);
+                    }
+                );
+            }
+            else if (!nfcSwitch.isChecked() && self.nfcEnabled) {
+                nfc.removeNdefListener(
+                    self.nfcHandler,
+                    function () {
+                        self.nfcEnabled = false;
+                    },
+                    function (error) {
+                        nfcSwitch.setChecked(true);
+                        document.getElementById('settingsError').textContent = error;
+                    }
+                );
+            }
+        };
+
+        self.nfcHandler = function (nfcEvent) {
+            navigator.notification.vibrate(100);
+            if (!self.nfcWriteMode)
+                window.plugins.spinnerDialog.show('', 'Reading NFC data', true);
+            var tag = nfcEvent.tag;
+            if (!self.nfcWriteMode) {
+                var nfcResult = "",
+                    ndefMessage = tag.ndefMessage;
+                for (var i = 0; i < ndefMessage.length; i++) {
+                    if (i > 0) {
+                        nfcResult += "<br/>";
+                    }
+                    var payload = nfc.bytesToString(ndefMessage[i].payload);
+                    if (84 == ndefMessage[i].type) {
+                        // for messages, strip the language prefix
+                        payload = payload.substring(3);
+                    }
+                    nfcResult += payload;
+                }
+                
+                document.getElementById('nfcInput').textContent = nfcResult;
+            }
+            else {
+                if (!tag.isWritable) { alert('tag not writable!'); }
+                else {
+                    var nfcRecords = [
+                        ndef.textRecord(new Date()),
+                        ndef.textRecord(document.getElementById('nfcInput').value),
+                        ndef.uriRecord("http://myoctopus.com")
+                    ]
+                    nfc.write(
+                        nfcRecords,
+                        function () { self.nfcWriteMode = false; alert('successfully written'); },
+                        function (e) { self.nfcWriteMode = false; alert(e); }
+                    );
+                }
+            }
+
+            window.plugins.spinnerDialog.hide();
+        }
+
+        self.nfcWriteMode = false;
+        self.writeNfc = function () {
+            self.nfcWriteMode = true;
+
+            // spinner with message, and a log message when it's dismissed
+            window.plugins.spinnerDialog.show(
+                null, // title
+                "Touch the NFC tag to write...", // message
+                function () {
+                    // dismissed
+                    self.nfcWriteMode = false;
+                }
+           );
+        }
+    };
+
+    window.myoctopus = new MyOctopus();
+    document.addEventListener('deviceready', window.myoctopus.onDeviceReady.bind(self), false);
+})();
