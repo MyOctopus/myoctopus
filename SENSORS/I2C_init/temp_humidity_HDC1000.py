79b20e76c593185d6b45be2ebe361f3e65567a2d
silvh <sylwester.h@gmail.com>
2015-05-31 16:55:06 +0100

    Update temp_humidity_HDC1000.py

--- SENSORS/I2C_init/temp_humidity_HDC1000.py
+++ SENSORS/I2C_init/temp_humidity_HDC1000.py
@@ -6,13 +6,29 @@ import time
 i2C = mraa.I2c(1);
 i2C.address(0x43); #hdc1000 address
 
+i2C.write(bytearray(b'\x02\x30\00')); #write config
+#0x02 - config adr
+#0x30 - Heater on, Mode of acquisition on, temperature 14bit, humudity 14bit
+#see register and memory map on HDC1000.pdf
 
-i2C.write(bytearray(b'\x00')); #mark temp from register
-time.sleep(0.02); #delay for get data
-d = i2C.read(4);
-print "HEX TEMP:%s" % binascii.hexlify(d); #print raw hex
+i2C.write(bytearray(b'\x02')); #read config
+d = i2C.read(2);
+print "CONFIG:%s" % binascii.hexlify(d);
+
+i2C.write(bytearray(b'\x00')); #temperature adr from register
+time.sleep(0.015); #delay for get data
+d = i2C.read(2);
+temp = int(binascii.hexlify(d),16)/65536.0*165.0-40.0;
+#print "HEX TEMP:%s" % binascii.hexlify(d); #print raw hex
+print "TEMPERATURE(-40:+125): %iC" % temp;
+
 
 i2C.write(bytearray(b'\x01')); #mark humidity from register    
-time.sleep(0.02);
+time.sleep(0.015);
 d = i2C.read(2);
-print "HEX HUMIDITY:%s" % binascii.hexlify(d); #print raw hex
+hum = int(binascii.hexlify(d),16)/65536.0*100.0;
+#print "HEX HUMIDITY: %s" % binascii.hexlify(d); #print raw hex
+print "HUMIDITY: %i %%rH" % hum;
+
+
+
