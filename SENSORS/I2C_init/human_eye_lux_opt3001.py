38657510a9099295ffad9196dfea0e367e52687a
silvh <sylwester.h@gmail.com>
2015-06-01 16:29:07 +0100

    Update human_eye_lux_opt3001.py

--- SENSORS/I2C_init/human_eye_lux_opt3001.py
+++ SENSORS/I2C_init/human_eye_lux_opt3001.py
@@ -9,9 +9,13 @@ i2C.address(0x45);
 
 i2C.write(bytearray(b'\x01\xc4\x10')); #write config
 #0x01 - config adr
-#0xc4 - full scale, 100ms conv. time, contiue measuring 
+#0xc4 - full scale, 100ms conv. time, contiue mesuring 
 #0x10 - latch 
-
-i2C.write(bytearray(b'\x00')); #light value register 
+i2C.write(bytearray(b'\x00')); #read light value 16bit
 d = i2C.read(2);
-print "RAW LUX %s" % binascii.hexlify(d); #print raw hex
+lraw = int(binascii.hexlify(d),16);
+
+res = lraw & 0x0FFF; #extracted fractional result mantissa 11:0 
+exp = (lraw>>12) & 0x000F; #extracted exponent 3:0 
+
+print 0.01*(2**(int(exp)))*int(res); #lux
