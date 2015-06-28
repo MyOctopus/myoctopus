d629580b7987a2089aa8b66ce1e8641ac1f289ef
larry <wawrzyniec@wawro.eu>
2015-06-28 01:32:01 +0100

    more comprehensive recipe and tar creation script

--- meta-myoctopus/generate_recipe.sh
+++ meta-myoctopus/generate_recipe.sh
@@ -9,7 +9,32 @@ echo $trimmed_source_dir
 dest_dir=$top_dir/meta-myoctopus/recipes-myoctopus/myoctopus/myoctopus
 mkdir -p $dest_dir
 
-folder_regex="s@$trimmed_source_dir@myoctopus_0.0.1@"
-tar -czvf $dest_dir/myoctopus_0.0.1.tar.gz --transform "$folder_regex" $source_dir
+folder_regex="s@$trimmed_source_dir@myoctopus_$1@"
+dest_file=$dest_dir/myoctopus_$1.tar.gz
 
+tar -czvf $dest_file --transform "$folder_regex" $source_dir
 
+md5s=$(md5sum  $dest_file | awk '{ print $1 }')
+sha256s=$(sha256sum  $dest_file | awk '{ print $1 }')
+#echo $md5s
+#echo $sha256s
+
+recipe_file=$top_dir/meta-myoctopus/recipes-myoctopus/myoctopus/myoctopus_$1.bb
+
+cat > $recipe_file << EOM
+SUMMARY = "My Octopus host software"
+LICENSE = "BSD"
+LIC_FILES_CHKSUM = "file://LICENSE;md5=b3cb8d8f023026f3a1204ea334572c80"
+
+SRC_URI = "file://myoctopus_\${PV}.tar.gz"
+SRC_URI[md5sum] = "$md5s"
+SRC_URI[sha256sum] = "$sha256s"
+
+S = "\${WORKDIR}/myoctopus_\${PV}"
+
+inherit setuptools
+
+RDEPENDS_\${PN} = "python-modules python-flask"
+EOM
+
+echo "Done"
